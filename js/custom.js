4ce339a7c0f7be18d4ee5011ca3c214f936e384a
CharlyTran <xShaluna@Charlys-MacBook-Pro.local>
2017-02-24 16:46:43 +0100

    redo TD1

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,6 @@
 var xmlhttp = new XMLHttpRequest();
+var xmlhttp1 = new XMLHttpRequest();
+
 xmlhttp.onreadystatechange = function() {
 if (this.readyState == 4 && this.status == 200) {
 var myObj = this.responseText;
@@ -8,3 +10,6 @@ document.getElementById("demo").innerHTML = jsonPretty;
 };
 xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
 xmlhttp.send();
+
+xmlhttp1.open("GET", "http://bitcoin.mubiz.com/info", true);
+xmlhttp1.send();
